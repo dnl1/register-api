63638a3390d6bf792ce03c7a9358849e5ac149e5
Danilo Oliveira Gomes <danilooliveira9@hotmail.com>
2019-07-28 12:09:38 -0300

    added enn controller

---
+++ Register.Api/Controllers/EnvironmentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Register.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvironmentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            return new
+            {
+                Environment.MachineName,
+                OSVersion = Environment.OSVersion.VersionString
+            };
+        }
+    }
+}
